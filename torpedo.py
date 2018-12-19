af221df9e576d9db7b7cd82eca521dd92b0e18c3
shaharraz <46004671+shaharraz@users.noreply.github.com>
2018-12-19 23:53:13 +0200

    Add files via upload

---
+++ torpedo.py
@@ -0,0 +1,7 @@
+class Torpedo:
+    def __init__(self, x, y, x_vel, y_vel, direction):
+        self.__x = x
+        self.__y = y
+        self.__x_vel = x_vel
+        self.__y_vel = y_vel
+        self.__direction = direction
